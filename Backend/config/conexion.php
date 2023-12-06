e2ef8f90ec3afc117bdcbd1fc07bbafe06bf0a96
xandraco <andraco7x7@outlook.com>
2023-12-06 17:59:34 -0600

    Added an online db

--- Backend/config/conexion.php
+++ Backend/config/conexion.php
@@ -1,9 +1,9 @@
 <?php
   function conectar() {
-    $host = "localhost:8889";
-    $user = "root";
-    $pass = "root";
-    $db = "crud_sistema";
+    $host = "auth-db531.hstgr.io";
+    $user = "u351541285_x_clone";
+    $pass = "1P0w3r#01";
+    $db = "u351541285_x_clone";
     $conn = mysqli_connect($host,$user,$pass);
     mysqli_select_db($conn, $db);
     return $conn;
