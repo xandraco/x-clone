874af5868aeaed78e9f7928a3e2cd1dc49b127d5
Catati64 <catati64@hotmail.com>
2023-12-06 13:10:41 -0600

    Send Responses - SUCCESS

--- Backend/Files/newRespond.php
+++ Backend/Files/newRespond.php
@@ -1,19 +1,30 @@
 <?php
-  include("../config/conexion.php");
-  $conn = conectar();
+include("../config/conexion.php");
+$conn = conectar();
+$dataPost = file_get_contents('php://input');
+$body = json_decode($dataPost, true);
 
-  $idPost = $_POST['idPost'];
-  $idUsuario = $_POST['idUsuario'];
-  $mensaje = $_POST['mensaje'];
-  $fecha = date("Y-m-d");
+$idPost = $body['idPost'];
+$idUsuario = $body['idUsuario'];
+$mensaje = $body['message'];
+$fecha = date("Y-m-d");
 
-  $queryInsert = "INSERT INTO posts VALUES (null, '$idUsuario','$titulo','$mensaje', '$fecha', '$reaccion')";
-  $result = mysqli_query($conn, $queryInsert);
+$querySelect = "SELECT id FROM `usuarios` WHERE email = '$idUsuario'";
+$idUserResult = mysqli_query($conn, $querySelect);
 
-  if ($result) {
-    echo json_encode(['STATUS' => 'SUCCESS', 'MESSAGE' => 'Se ha registrado el reponse']);
-    Header("Location: ../../home.html?usuario=".$idUsuario);
-  } else {
-    echo json_encode(['STATUS' => 'ERROR', 'MESSAGE' => 'No se ha registrado el response']);
-  }
+if ($idUserResult) {
+    $idUserArray = mysqli_fetch_assoc($idUserResult);
+    $idFinal = $idUserArray['id'];
+    
+    $queryInsert = "INSERT INTO responses VALUES (null, '$idPost', '$idFinal', '$mensaje')";
+    $result = mysqli_query($conn, $queryInsert);
+
+    if ($result) {
+        echo json_encode(['STATUS' => 'SUCCESS', 'MESSAGE' => 'Se ha registrado el response']);
+    } else {
+        echo json_encode(['STATUS' => 'ERROR', 'MESSAGE' => $queryInsert]);
+    }
+} else {
+    echo json_encode(['STATUS' => 'ERROR', 'MESSAGE' => 'No se encontró el usuario']);
+}
 ?>
